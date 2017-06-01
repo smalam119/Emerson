b30d2b788d51a98d1786465b40b7ce813a11f9ee
MacBook Pro 13 <smalam119@gmail.com>
2017-06-02 00:24:43 +0600

    ui and view binding completed

---
+++ EmersonQuotes/QuotesViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  EmersonQuotes
+//
+//  Created by MacBook Pro 13" on 6/1/17.
+//  Copyright © 2017 smalam119. All rights reserved.
+//
+
+import UIKit
+
+class QuotesViewController: UIViewController {
+    
+    @IBOutlet weak var quoteNumberLabel: UILabel!
+    @IBOutlet weak var quoteOneView: UIView!
+    @IBOutlet weak var quoteTwoView: UIView!
+    @IBOutlet weak var quoteThreeView: UIView!
+    
+    @IBAction func onPressBackButton(_ sender: Any) {
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad();
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning();
+    }
+
+
+}
+
