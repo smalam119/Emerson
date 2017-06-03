c31736f6b7baf96ee80043efaa69720a8d1be0a3
MacBook Pro 13 <smalam119@gmail.com>
2017-06-04 00:37:50 +0600

    comments added

--- EmersonQuotes/QuotesViewController.swift
+++ EmersonQuotes/QuotesViewController.swift
@@ -19,15 +19,19 @@ class QuotesViewController: UIViewController {
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var backButton: UIButton!
     
+    @IBAction func onPressNextButton(_ sender: Any) {
+        
+        //increment index and push an item into observable stack
+        //on next button click
+        quoteViewIndex += 1
+        observableStack.push((quoteViewArray?[quoteViewIndex])!)
+    }
+    
+    //decrement index hide the top item and pop
     @IBAction func onPressBackButton(_ sender: Any) {
         hideUIView(uiView: (quoteViewArray?[quoteViewIndex])!)
         quoteViewIndex -= 1
         observableStack.pop()
-    }
-    
-    @IBAction func onPressNextButton(_ sender: Any) {
-        quoteViewIndex += 1
-        observableStack.push((quoteViewArray?[quoteViewIndex])!)
     }
     
     override func viewDidLoad() {
@@ -37,12 +41,16 @@ class QuotesViewController: UIViewController {
         makeAllViewInvisible()
         observableStack.push((quoteViewArray?[quoteViewIndex])!)
         
+        //subscribe to observable array
+        //observer block
         _ = observableStack.array.rx_elements().subscribe { event in
             switch event {
             case .next(let value):
+                //show show or hide item when pushed or poped from observable stack
                 self.showUIView(uiView: value.last!)
                 self.updateNavBarLabel(number: self.quoteViewIndex + 1)
                 
+                //controlling back and next button appearence and interection
                 if(self.observableStack.getSize() == 1) {
                     self.disable(button: self.backButton)
                 } else if(self.observableStack.getSize() == (self.quoteViewArray?.count)!) {
@@ -65,16 +73,19 @@ class QuotesViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
+    //initially make all view invisible
     func makeAllViewInvisible() {
         for view in quoteViewArray! {
             view.isHidden = true
         }
     }
     
+    //show a particular view
     func showUIView(uiView: UIView) {
         uiView.isHidden = false
     }
     
+    //hide a view
     func hideUIView(uiView: UIView) {
         uiView.isHidden = true
     }
@@ -83,16 +94,19 @@ class QuotesViewController: UIViewController {
         quoteNumberLabel.text = "Quotes No.\(number)"
     }
     
+    //disable a button
     func disable(button: UIButton) {
         button.setImage(nil, for: .normal)
         button.isUserInteractionEnabled = false
     }
     
+    //enable back button
     func enableBackButton() {
         backButton.setImage(UIImage(named: "back") , for: .normal)
         backButton.isUserInteractionEnabled = true
     }
     
+    //enable next button
     func enableNextButton() {
         nextButton.setImage(UIImage(named: "next") , for: .normal)
         nextButton.isUserInteractionEnabled = true
--- EmersonQuotes/Stack.swift
+++ EmersonQuotes/Stack.swift
@@ -13,19 +13,23 @@ import UIKit
 struct Stack {
     var array: ObservableArray<UIView> = []
     
+    //function to push a item into the observable array
     mutating func push(_ uiView: UIView) {
         array.append(uiView);
     }
     
+    //function to pop a item
     mutating func pop() {
         array.popLast();
     }
     
+    //check the top most item
     func peek() -> UIView? {
         return array.count != 0 ? array[array.count-1] : nil;
         
     }
     
+    //get the array size
     func getSize() -> Int {
         return array.count;
     }
